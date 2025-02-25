d5ca80b6c69164c35a1b4937aa73f0e5c9fcea76
sathyathangavel2022 <sathyathangavel2022@gmail.com>
2025-02-25 10:18:03 +0000

    second commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     """Example Hello World route."""
-    name = os.environ.get("NAME", "Nivin Surya and Geethu")
+    name = os.environ.get("NAME", "Hello Team")
     return f"Hello {name}!"
 
 
